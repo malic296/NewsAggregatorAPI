bdacd9eb6ecfb423f3a97c268534b8f48f5a171a
malic296 <janmalek21JM@gmail.com>
2026-03-30 17:07:48 +0200

    Added better error handling logic

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI, HTTPException
+from starlette.status import HTTP_400_BAD_REQUEST
+
 from app.api.v1.article import article_router_v1
 from app.api.v1.channel import channel_router_v1
 from app.api.v2.article import article_router
@@ -11,6 +13,7 @@ from fastapi.responses import JSONResponse
 from app.dependencies.logging import get_logging_handler
 from app.handlers import LoggingHandler
 from app.dependencies.auth import get_rate_limiter
+from app.schemas.responses import BaseResponse
 
 #USE FOR TESTS
 #app = FastAPI()
@@ -18,10 +21,25 @@ from app.dependencies.auth import get_rate_limiter
 #USE FOR PRODUCTION
 app = FastAPI(dependencies=[Depends(get_rate_limiter())])
 
+def create_error_response(message: str, status_code: int) -> JSONResponse:
+    response: BaseResponse = BaseResponse(
+        success=False,
+        message=message,
+        status_code=status_code
+    )
+
+    return JSONResponse(
+        status_code=status_code,
+        content=response.model_dump()
+    )
+
 @app.exception_handler(Exception)
 async def global_exception_handler(request: Request, err: Exception):
     logger: LoggingHandler = get_logging_handler()
+
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    message = "API failed unexpectedly"
+
     if isinstance(err, InternalError):
         if err.internal_message:
             logger.handle(err.internal_message)
@@ -30,37 +48,18 @@ async def global_exception_handler(request: Request, err: Exception):
         message = err.public_message
 
     else:
-        message = f"API failed unexpectedly"
-        logger.handle(f"{message} because: {str(err)} for {request.method} {request.url}")
+        logger.handle(f"Unexpected Error: {str(err)} | Path: {request.url.path}")
 
-    return JSONResponse(
-        status_code=status_code,
-        content={
-            "success": False,
-            "message": message
-        }
-    )
+    return create_error_response(message = message, status_code = status_code)
+
 
 @app.exception_handler(HTTPException)
 async def http_exception_handler(request: Request, err: HTTPException):
+    message = err.detail
     if err.status_code == status.HTTP_429_TOO_MANY_REQUESTS:
         message = "You have exceeded the rate limit."
-        return JSONResponse(
-            status_code=status.HTTP_429_TOO_MANY_REQUESTS,
-            content={
-                "success": False,
-                "message": message
-            }
-        )
 
-    return JSONResponse(
-        status_code=err.status_code,
-        content={
-            "success": False,
-            "message": err.detail
-        }
-    )
-
+    return create_error_response(message = message, status_code = err.status_code)
 
 app.include_router(article_router, prefix="/latest")
 app.include_router(channel_router, prefix="/latest")
